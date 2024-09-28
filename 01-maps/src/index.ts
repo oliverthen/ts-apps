f43406c4f8cca87c09097eaca57a7fcc02d461a1
Oliver Then <oli171dr@gmail.com>
2024-09-28 13:16:47 -0400

    Added User and Company classes and made use of faker library to generate fake data

--- 01-maps/src/index.ts
+++ 01-maps/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello world')
+import { User } from "./User";
+import { Company } from "./Company";
+
+const user = new User();
+
+console.log(user);
+
+const company = new Company();
+
+console.log(company);
