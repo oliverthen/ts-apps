dfb610531c04ecbb2288370dfcb1dcb5ed0e5b18
Oliver Then <oli171dr@gmail.com>
2024-10-02 11:38:40 -0400

    Implement String class that complies with Sortable interface

--- 02-sort/src/index.ts
+++ 02-sort/src/index.ts
@@ -1,10 +1,19 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0])
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0])
 
-const sorter = new Sorter(numbersCollection);
+
+// const sorter = new Sorter(numbersCollection);
+
+// sorter.sort();
+
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaayb');
+
+const sorter = new Sorter(charactersCollection);
 
 sorter.sort();
-
-console.log(numbersCollection.data);
+console.log(charactersCollection.data);
