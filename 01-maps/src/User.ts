0d8e49e9d0ae8ed25080ca10eb5dc3b92edc53d9
Oliver Then <oli171dr@gmail.com>
2024-10-01 20:01:51 -0400

    Set up Sorter App, typescript support, and initial sorting algorithm with array of numbers

--- 01-maps/src/User.ts
+++ 01-maps/src/User.ts
@@ -1,13 +1,15 @@
 import { faker } from "@faker-js/faker";
+import { Mappable } from "./CustomMap";
 
 
 
-export class User {
+export class User implements Mappable {
 	name: string;
 	location: {
 		lat: number;
 		lng: number;
 	};
+	color: string = 'red';
 
 	constructor() {
 		this.name = faker.name.firstName();
@@ -16,4 +18,9 @@ export class User {
 			lng: parseFloat(faker.address.longitude())
 		}
 	}
+
+	markerContent(): string {
+		return `User Name: ${this.name}`
+	}
+
 }
---
+++ 02-sort/src/index.ts
@@ -0,0 +1,10 @@
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+
+const numbersCollection = new NumbersCollection([10, 3, -5, 0])
+
+const sorter = new Sorter(numbersCollection);
+
+sorter.sort();
+
+console.log(numbersCollection.data);
