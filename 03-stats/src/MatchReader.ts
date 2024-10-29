916e77696fa4b0edbdd3e35bc0fb325b6b284f27
Oliver Then <oli171dr@gmail.com>
2024-10-29 10:50:31 -0400

    Finished adding report classes

--- 03-stats/src/MatchReader.ts
+++ 03-stats/src/MatchReader.ts
@@ -1,6 +1,7 @@
 import { dateStringToDate } from './utils';
 import { MatchResult } from './MatchResult';
 import { MatchData } from './MatchData';
+import { CsvFileReader } from './CsvFileReader';
 
 
 
@@ -10,6 +11,10 @@ interface DataReader {
 }
 
 export class MatchReader {
+	static fromCsv(filename: string): MatchReader {
+		return new MatchReader(new CsvFileReader(filename));
+	}
+
 	matches: MatchData[] = [];
 
 	constructor(public reader: DataReader) {}
--- 03-stats/src/index.ts
+++ 03-stats/src/index.ts
@@ -1,16 +1,9 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
 import { Summary } from './Summary';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
 
-// Creae an object that satisfies the DataReader interface
 
-const csvFileReader = new CsvFileReader('football.csv');
-
-// Create an instace of MatchReader and pass in something satisfying the DataReader interface
-const matchReader = new MatchReader(csvFileReader)
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
+const summary = Summary.winsAnalysisWithHtmlReport('Man United')
 summary.buildAndPrintReport(matchReader.matches);
