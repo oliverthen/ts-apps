56d5980739b39c0c81e99f3e6a2cfc7e6fb80dd2
Oliver Then <oli171dr@gmail.com>
2024-11-02 17:19:30 -0400

    additional work with User model

--- 04-web/src/index.ts
+++ 04-web/src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hi there');
+import { User } from "./models/User";
+
+const user = new User({ name: 'myname', age: 20});
+console.log(user.get('name'));
+console.log(user.get('age'));
+user.set({name: 'Oliver'});
+console.log(user.get('name'));
+console.log(user.get('age'));
