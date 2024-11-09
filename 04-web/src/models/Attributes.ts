47628852dad20bb1cccc03dc7c801173e1a6cd52
Oliver Then <oli171dr@gmail.com>
2024-11-09 10:22:41 -0500

    Adjusted Generic Type for Sync Class and started work on Attributes class

---
+++ 04-web/src/models/Attributes.ts
@@ -0,0 +1,13 @@
+
+
+export class Attributes<T extends object> {
+	constructor(private data: T) {}
+
+	get(propName: string): (number | string) {
+		return this.data[propName];
+	}
+
+	set(update: T): void {
+		Object.assign(this.data, update);
+	}
+}
