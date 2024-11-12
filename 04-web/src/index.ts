3121c4364a113008527ab8197f194db14e6a306e
Oliver Then <oli171dr@gmail.com>
2024-11-12 18:42:22 -0500

    Added fetch and save methods to User class to take advantage of Sync methods

--- 04-web/src/index.ts
+++ 04-web/src/index.ts
@@ -1,8 +1,11 @@
 import { User } from "./models/User";
 
-const user = new User({name: 'new record', age: 0});
+const user = new User({id: 2, name: 'Oliver', age: 31});
 
-console.log(user);
+user.on('save', () => {
+	console.log(user);
+});
 
-console.log(user.get('age'));
+user.save();
+
 
