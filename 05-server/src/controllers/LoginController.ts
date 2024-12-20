24f89c30b33c606b49e6d20d1392ba3af9eff1f5
Oliver Then <oli171dr@gmail.com>
2024-12-20 10:16:13 -0500

    Set up middleware capability

--- 05-server/src/controllers/LoginController.ts
+++ 05-server/src/controllers/LoginController.ts
@@ -1,5 +1,6 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { get, controller } from './decorators';
+
 
 @controller('/auth')
 class LoginController {
