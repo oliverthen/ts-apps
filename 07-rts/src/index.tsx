f0fbd4d7ffb5ee7f91cc18b47fc8d521ab241a05
Oliver Then <oli171dr@gmail.com>
2025-02-06 07:28:19 -0500

    Working with props and interfaces

--- 07-rts/src/index.tsx
+++ 07-rts/src/index.tsx
@@ -1,13 +1,16 @@
 import ReactDOM from "react-dom/client";
+import Parent from "./props/Parent";
+
 
 const el = document.getElementById("root");
 
 const root = ReactDOM.createRoot(el!);
 
+
 const App = () => {
   return (
     <div>
-      <h1>Hi there!</h1>
+      <Parent />
     </div>
   );
 };
